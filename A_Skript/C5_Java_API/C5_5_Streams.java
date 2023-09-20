4476a7f0d38a85dceb1d41d4ebbb37c2016311c7
Komodorpudel <sah_services@outlook.com>
2023-09-20 12:43:57 +0200

    sad

--- A_Skript/C5_Java_API/C5_5_Streams.java
+++ A_Skript/C5_Java_API/C5_5_Streams.java
@@ -44,6 +44,28 @@ public class C5_5_Streams {
 
         System.out.println(myArrayList);
 
+
+        /*         
+        Casting int to char (and vice-versa):
+        Both int and char are primitive types in Java.
+        A char represents a single 16-bit Unicode character and has a range of values from 0 to 65,535 inclusive.
+        An int is a 32-bit signed integer, with a much larger range.
+        When you cast an int to a char, you are effectively reducing its size and taking the lowest 16 bits of the integer. If the int value is within the valid range for a char, the cast works as you might expect. If the int is outside the range for char, the value will be truncated.
+
+        Casting Between Other Primitives:
+        You can cast between other primitives as well, like double to int (which truncates the decimal), long to int (which may result in loss of data), etc.
+        However, implicit narrowing conversions (where you might lose information, like from long to int) need an explicit cast, while widening conversions (like from int to long) do not.
+
+        Casting with Objects:
+        Casting among objects follows a different set of rules than primitives.
+        You can only cast between objects if there's a valid inheritance relationship (either superclass to subclass or vice-versa).
+        If you try to cast between unrelated classes, you'll get a compile-time error. Even if you bypass that, at runtime, you'd get a ClassCastException.
+
+        Primitive Wrappers:
+        Java has wrapper classes for each primitive type (e.g., Integer for int, Character for char).
+        You can't cast between unrelated wrapper objects directly. For example, you can't cast an Integer object to a Character object. You'd need to first convert to primitives and then perform the cast.
+        */
+
         // S56 - Stream
         System.out.println("\nStream:");
 
